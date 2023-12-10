9bb46cb654d879953b70894f8ea737d416343901
JohannaB95 <johanna_b_g@hotmail.com>
2023-12-10 12:34:36 -0500

    Documentación del código

--- app.js
+++ app.js
@@ -1,23 +1,33 @@
+//Se carga la libreria colors para permitir el uso de estilos de colores en la terminal
 require('colors');
 
+//Se carga el código que contiene el archivo math.js en la carpeta modules.
 const math = require('./modules/math.js')
 
+//Se limpia la consola antes de imprimir la interfaz que contiene las funciones matemáticas
 console.clear();
+
+//Se imprime en la consola el contenido del objeto math
 console.log(math);
 
+//Se declara una función flecha main asincrónica 
 const main = async() => {
+
+    //Se imprime en la consola un recuadro con un título
     console.log('*****************************'.blue);
     console.log('*  '.blue,'Funciones Matematicas'.bgGreen, '  *'.blue);
 
     console.log('*****************************\n'.blue);
+    /*Se imprime en la consola un recuadro con los resultados de cada una de las operaciones mátematicas que
+    se realizaran*/
     console.log('*****************************'.yellow);
     console.log('*                           *'.yellow);
 
     console.log('*             '.yellow + math.add(5,3) + '             *'.yellow );
-    console.log('*            '.yellow + math.substract(2,8) + '             *'.yellow );
+    console.log('*            '.yellow + math.subtract(2,8) + '             *'.yellow );
     console.log('*            '.yellow + math.multiply(2,8) + '             *'.yellow );
     console.log('*    '.yellow + math.divide(2,0.9) + '     *'.yellow );
     console.log('*****************************'.yellow);
 }
-
+//Se llama a la función main
 main();
--- modules/math.js
+++ modules/math.js
@@ -1,29 +1,45 @@
+//Se declara un objeto vacío llamado Math. Este objeto se utiliza para agrupar funciones relacionadas con operaciones matemáticas.
 const Math = {};
 
+//Se declara una función llamada add con dos parámetros a y b
 function add(a,b) {
+    //Se retorna el valor de la suma entre los parámetros a y b
     return a + b;
 }
 
-function substract(a,b) {
+//Se declara una función llamada subtract con dos parámetros a y b
+function subtract(a,b) {
+    //Se retorna el valor de la resta entre los parámetros a y b
     return a - b;
 }
 
+//Se declara una función llamada multiply con dos parámetros a y b
 function multiply(a,b) {
+    //Se retorna el valor de la multiplicación entre los parámetros a y b
     return a * b;
 }
 
+//Se declara una función llamada divide con dos parámetros a y b
 function divide(a,b) {
+    //Se utiliza if para verificar si b es igual a 0 y si a es igual a 0
     if (b == 0 && a == 0) {
+        //Si la condición se cumple, se retornara un mensaje de error que indica que no se puede dividir entre cero
         return "Error: no se puede dividir entre cero"
+    //Y si b es igual a cero
     } else if (b == 0) {
+        //Si la condición se cumple, se retornara un mensaje de error que indica que no se puede dividir entre cero
         return "Error: no se puede dividir entre cero" 
     }
+        //Si ninguna de las dos condiciones se cumple se retorna el resultado de la división entre a y b
         return a / b;
     }
 
+/*Se asignan las funciones add, subtract, multiply, y divide como propiedades del objeto Math. Esto crea 
+métodos asociados con el objeto Math.*/
 Math.add = add;
-Math.substract = substract;
+Math.subtract = subtract;
 Math.multiply = multiply;
 Math.divide = divide;
 
+//Se exporta el objeto Math para que pueda ser utilizado en otros archivos. Esto se logra usando module.exports.
 module.exports = Math;
